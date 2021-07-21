8bbbf5ae78f054b6337e7ea0414f787075658e6a
riadhamami <riadh.amami@esprit.tn>
2021-07-21 19:07:54 +0100

    fixing shit

--- vars/BuildImage.groovy
+++ vars/BuildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName,String dockerRep) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t docker.galactechstudio.com/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login https://docker.galactechstudio.com -u $USER --password-stdin"
+        sh "docker build -t $imageName ."
+        sh "echo $PASS | docker login $dockerRep -u $USER --password-stdin"
         sh 'docker push docker.galactechstudio.com/demo-app:jma-2.0'
     }
 
