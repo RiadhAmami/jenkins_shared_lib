679a65571516b7425f1972dbb21cc855745758c2
riadhamami <riadh.amami@esprit.tn>
2021-07-21 18:34:55 +0100

    fixing shit

---
+++ vars/BuildImage.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+def call() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t nexus.galactechstudio.com/demo-app:jma-2.0 .'
+        sh "echo $PASS | docker login https://docker.galactechstudio.com -u $USER --password-stdin"
+        sh 'docker push nexus.galactechstudio.com/demo-app:jma-2.0'
+    }
+}
